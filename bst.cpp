b36b1dcea59a114a4feaee2ca0c22bec0e12a613
Jared McCain <jaredmccain.jam@gmail.com>
2023-03-31 20:22:02 -0500

    FINAL CORRECTIONS

--- bst.cpp
+++ bst.cpp
@@ -8,7 +8,7 @@ int main(){
     //BST bst2;
     Node *root = nullptr;
     const int nums = 7;
-    /*for (int i = 0; i < nums; i++){
+    for (int i = 0; i < nums; i++){
         int k = rand() % 10000 + 1;
         root = bst.insert(root, k);
     }
@@ -17,11 +17,11 @@ int main(){
     cout << "\nInorder traversal of the given tree\n";
     bst.inOrder(root);
     cout << "\nPostorder traversal of the given tree\n";
-    bst.postOrder(root);*/
+    bst.postOrder(root);
 
-    //bst2 is testing
+    /*bst2 is testing
     Node *root2 = nullptr;
-    /*root2 = bst2.insert(root2, 50);
+    root2 = bst2.insert(root2, 50);
     root2 = bst2.insert(root2, 30);
     root2 = bst2.insert(root2, 20);
     root2 = bst2.insert(root2, 40);
